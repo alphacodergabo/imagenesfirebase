0acc8646e2d29b54487a8216c87f324d08361425
alphacodergabo <gabo.9218@gmail.com>
2020-05-19 18:07:35 -0500

    repo subir imagenes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demoFile';
+  uploadPercent: Observable<number>;
+  downloadURL: Observable<string>;
+  title = 'app';
+  constructor(private storage: AngularFireStorage) { }
+
+  uploadFile(event) {
+    const file = event.target.files[0];
+    const filePath = 'Imagenesprueba';
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+    
+    // observe percentage changes
+    this.uploadPercent = task.percentageChanges();
+    // get notified when the download URL is available
+    task.snapshotChanges().pipe(
+      finalize(() => this.downloadURL = fileRef.getDownloadURL() )
+   )
+  .subscribe()
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDSJiEUBFPdvavsP2bqnk3nCEQfSw-x6Cg',
+    authDomain: 'imagenesprueba-569ed.firebaseapp.com',
+    databaseURL: 'https://imagenesprueba-569ed.firebaseio.com',
+    projectId: 'imagenesprueba-569ed',
+    storageBucket: 'imagenesprueba-569ed.appspot.com',
+    messagingSenderId: '700803797704',
+    appId: '1:700803797704:web:5d39a870f2f58e66df8bfd',
+    measurementId: 'G-7MHP6R7708'
+  }
 };
 
 /*
